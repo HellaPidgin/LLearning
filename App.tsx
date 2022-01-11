52d97a61ab2e0ae72dfec61e8dd094200c245c82
Samuel <s.okoro07@gmail.com>
2022-01-11 14:09:27 +0100

    Reading from firebase

--- App.tsx
+++ App.tsx
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { config } from './config';
+import {initializeApp} from "firebase/app";
+import { getDatabase, ref, onValue, Database } from "firebase/database";
+import { useEffect } from 'react';
+
+initializeApp(config);
+
 
 export default function App() {
+  const courseListener = async () => {
+    const db = getDatabase();
+    console.log("Hello World")
+    const reference = ref(db, "course");
+    //console.log(reference)
+    await onValue(reference, (snapshot) => {
+      console.log(snapshot)
+      console.log(snapshot.val());
+    });
+  };
+
+
+  useEffect(() => {
+    courseListener();
+  }, [])
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyBCdsgs7huJ90ucIeEjzbLcBM3PGy8PbX0",
+  authDomain: "decoded-reducer-310510.firebaseapp.com",
+  databaseURL: "https://decoded-reducer-310510-default-rtdb.firebaseio.com",
+  projectId: "decoded-reducer-310510",
+  storageBucket: "decoded-reducer-310510.appspot.com",
+  messagingSenderId: "792842708021",
+  appId: "1:792842708021:web:cb9ca3092ceac3b0b83d7c",
+  measurementId: "G-SMZ2X5JDFC",
+};
